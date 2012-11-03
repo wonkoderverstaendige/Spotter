6c35362e52488d4b5e8a73e9df0849a8d894813c
wonko <crangos@web.de>
2012-11-03 02:21:50 -0400

    Docopt for Writer. Tracker not working.

--- ledtrack.py
+++ ledtrack.py
@@ -5,15 +5,15 @@ Created on Mon Jul 09 00:02:20 2012
 
 Track position LEDs and sync signal from camera or video file.
 
-Usage: 
-    ledtrack.py --source SRC [--outfile OUT] [options]
+Usage:
+    ledtrack.py --source SRC [--outfile DST] [options]
     ledtrack.py -h | --help
-    
+
 Options:
     -h --help           Show this screen
     -f --fps FPS        Fps for camera and video
     -s --source SRC     Path to file or device ID [default: 0]
-    -o --outfile OUT    Path to video out file [default: None]
+    -o --outfile DST    Path to video out file [default: None]
     -d --dims DIMS      Frame size [default: 320x200]
     -H --Headless       Run without interface
     -D --DEBUG          Verbose output
@@ -41,7 +41,7 @@ global DEBUG
 class Main(object):
     grabber = None
     writer = None
-    
+
     paused = False
     windowName = 'Capture'
     viewMode = 0
@@ -52,45 +52,44 @@ class Main(object):
     selection = None
     drag_start = None
     record_to_file = True
-    
-    
+
+
     def __init__( self, source, destination, fps, size, gui='cv2.highgui' ):
-        
-        # Setup frame grab object, fills framebuffer        
+
+        # Setup frame grab object, fills framebuffer
         self.grabber = grabber.Grabber( source, fps, size )
-        
-        # Setup writer if required, writes frames from buffer to video file.    
+
+        # Setup writer if required, writes frames from buffer to video file.
         if destination:
-            # instantiate video writer object
-            self.writer = writer.Writer( destination, fps, size )
-        
+            self.writer = writer.Writer( destination, self.grabber.fps, self.grabber.size )
+
         # tracker object finds LEDs in frames
         self.tracker = tracker.Tracker()
-        
+
         # Only OpenCV's HighGui is currently used as user interface
         if gui=='cv2.highgui':
             cv2.namedWindow( self.windowName )
             cv2.setMouseCallback( self.windowName, self.onMouse )
-            
+
         # histogram instance required to do... what, again?
         self.hist = utils.HSVHist()
 
 
     def onMouse( self, event, x, y, flags, param ):
-        """ 
+        """
         Mouse interactions with Main window:
             - Left mouse click gives pixel data under cursor
             - Left mouse drag selects rectangle
-            
+
             - Right mouse button switches view mode
         """
         if event == cv2.EVENT_LBUTTONDOWN:
             if not self.current_frame == None:
                 self.drag_start = (x, y)
-                
+
         if event == cv2.EVENT_LBUTTONUP:
                 self.drag_start = None
-                
+
                 if self.selection == None:
                     pixel = self.current_frame[y, x]
                     print "[X,Y][B G R](H, S, V):", [x, y], pixel, utils.BGRpix2HSV(pixel)
@@ -103,20 +102,22 @@ class Main(object):
             ymin = min( y, self.drag_start[1] )
             xmax = max( x, self.drag_start[0] )
             ymax = max( y, self.drag_start[1] )
-            
+
             if xmax - xmin < 2 and ymax - ymin < 2:
                 self.selection = None
             else:
                 self.selection = ( xmin, ymin, xmax - xmin, ymax - ymin )
-                
+
         if event == cv2.EVENT_RBUTTONDOWN:
             self.nextView()
-            
+
+
     def nextView( self ):
         self.viewMode += 1
         if self.viewMode > self.nviewModes:
             self.viewMode = 0
-    
+
+
     def update_frame( self ):
 
 #        utils.drawCross( self.current_frame, 100, 100, 10, (100, 255, 255) )
@@ -126,18 +127,18 @@ class Main(object):
             colors = [(0, 0, 255), (0, 255, 0), (255, 0, 0)]
             for cf, coord in enumerate(ledcoords):
                 utils.drawCross( self.show_frame, coord[0], coord[1], 5, colors[cf], gap = 3 )
-                
+
         elif self.viewMode == 1:
             self.show_frame = cv2.bitwise_and( self.current_frame, self.current_frame, mask = self.tracker.mask )
-            
+
     def show( self ):
         cv2.imshow( self.windowName, self.show_frame )
-        
+
     def updateHist( self ):
         self.hist.calcHist( self.hsv_frame )
         self.hist.overlayHistMap()
         cv2.imshow( 'histogram', self.hist.overlay )
-        
+
     def trackLeds( self ):
         self.tracker.sumTrack( self.current_frame )
 
@@ -148,29 +149,29 @@ class Main(object):
 
 if __name__ == "__main__":
 
-    # Command line parsing    
+    # Command line parsing
     ARGDICT = docopt( __doc__, version=None )
     DEBUG   = ARGDICT['--DEBUG']
     if DEBUG: print( ARGDICT )
 
     # no GUI, may later select GUI backend, i.e., Qt or cv2.highgui etc.
     gui = 'cv2.highgui' if not ARGDICT['--Headless'] else ARGDICT['--Headless']
-     
-    # Frame size parameter string 'WIDTHxHEIGHT' to size list [WIDTH, HEIGHT]
-    size = [0, 0] if not ARGDICT['--dims'] else list( ARGDICT['--dims'].split('x') )
-        
+
+    # Frame size parameter string 'WIDTHxHEIGHT' to size tupple (WIDTH, HEIGHT)
+    size = (0, 0) if not ARGDICT['--dims'] else tuple( ARGDICT['--dims'].split('x') )
+
     # Instantiating main class ... no shit, Sherlock!
     main = Main( source      = ARGDICT['--source'],
                  destination = utils.dst_file_name( ARGDICT['--outfile'] ),
-                 fps         = ARGDICT['--fps'], 
-                 size        = size, 
+                 fps         = ARGDICT['--fps'],
+                 size        = size,
                  gui         = gui )
 
 
     # Event for Thread wake-up, required even if no writer Thread present
     frame_event = threading.Event()
     # seperate video writer thread if Writer Instantiated
-    if main.writer:    
+    if main.writer:
         writer_thread = threading.Thread( target = main.writer.write_thread, args = ( main.grabber.framebuffer, frame_event, ) )
         writer_thread.start()
 
@@ -180,13 +181,13 @@ if __name__ == "__main__":
         t = int( 1000/main.grabber.fps )
     else:
         t = 33
-        
+
     ts_start = time.clock()
     while True:
         if main.grabber.grab_next():
             frame_event.set()
             frame_event.clear()
-            
+
             if not main.paused:
                 main.current_frame = main.grabber.framebuffer[0]
                 main.hsv_frame = cv2.cvtColor( main.current_frame, cv2.COLOR_BGR2HSV )
@@ -199,27 +200,26 @@ if __name__ == "__main__":
         t = int( 1000/main.grabber.fps - total_elapsed ) - 1
         if t <= 0:
             if DEBUG: print 'Missed next frame by: ' + str( t * -1. ) + ' ms'
-            t = 1        
-        
+            t = 1
+
         key = cv2.waitKey(t)
-        
+
         if ( key % 0x100 == 32 ):
             main.paused = not main.paused
-        
+
         # escape key closes windows/exits
         if ( key % 0x100 == 27 ):
-            if DEBUG: print 'Exiting...'            
-            
+            if DEBUG: print 'Exiting...'
+
             # wake up threads to let them stop themselves
-            if main.record_to_file:            
-                main.writer._alive = False
+            if main.record_to_file:
+                main.writer.alive = False
                 frame_event.set()
                 #main.writer.close()
-            
-            main.grabber.close()
 
+            main.grabber.close()
             cv2.destroyAllWindows()
-            
+
             fc = main.grabber.framecount
             tt = ( time.clock() - ts_start )
             print 'Done! Grabbed ' + str( fc ) + ' frames in ' + str( tt ) + 's, with ' + str( fc / tt ) + ' fps'
--- lib/grabber.py
+++ lib/grabber.py
@@ -7,19 +7,18 @@ Handles grabbing successive frames from capture object and returns current
 frames of requested type.
 Capture object must be closed with Grabber.close() or script will not terminate!
 
-Usage: 
+Usage:
     grabber.py --source SRC [--dims DIMS --fps FPS -D]
     grabber.py -h | --help
 
 Options:
-    -h --help          Show this screen
-    -f --fps=<FPS>     Fps for camera and video
-    -s --source=<SRC>  Source, path to file or integer device ID [default: 0]
-    -d --dims=<DIMS>   Frame size [default: 320x200]
-    -D --DEBUG         Verbose debug output
+    -h --help        Show this screen
+    -f --fps FPS     Fps for camera and video
+    -s --source SRC  Source, path to file or integer device ID [default: 0]
+    -d --dims DIMS   Frame size [default: 320x200]
+    -D --DEBUG       Verbose debug output
 
 """
-
 
 import cv, cv2, time, os, sys
 sys.path.append('./lib')
@@ -31,122 +30,129 @@ DEBUG = True
 class Grabber:
     capture = None          # Capture object to frame source
     fourcc = None           # Source frame coding
-    fps = None              # Current source fps, may differ from CLI parameter
+
+    fps_init = None              # Current source fps, may differ from CLI parameter
+    fps = None
+
+    size_init = None
+    size = None
+
     framecount = 0          # Frames received so far
     ts_last_frame = None    # Timestamp of most recent frame
     ts_first = None         # Timestamp of first frame, BUGGY!
     source_type = None      # File, stream, device; changes behavior of GUI
-    framebuffer = deque(maxlen=256)
-    
-    def __init__( self, source, fps=0, size=[0, 0] ):
+    framebuffer = deque( maxlen=256 )
+
+    def __init__( self, source, fps=0, size=(0, 0) ):
         """ source - Integer DeviceID or path to source file
             fps    - Float, frames per second of replay/capture
-            size   - lsit of floats (width, height)
+            size   - list of floats (width, height)
         """
 
         # Integer: Devide ID. Else, grabber will use as path and look for file
         try:
             source = int( source )
             self.source_type = 'device'
-            if DEBUG: print 'Source type: device.'
         except ValueError:
             self.source_type = 'file'
             if not os.path.isfile( source ):
-                print 'Unable to open source file!'
+                print 'Source file ' + source + ' does not exist.'
                 sys.exit(0)
-       
+
         # Creating capture handle object
         if DEBUG: print 'Attempting to open source "' + str( source ) + '" of type ' + self.source_type +  ' as capture... '
-        try:                
+        try:
             self.capture = cv2.VideoCapture( source )
         except:
-            print 'Unable to open Capture!'
+            print 'Unable to open VideoCapture!'
         if DEBUG: print str( self.capture ) + ' returned.'
-        
+
         # Proper fps values only important if lower than what camera can provide,
-        # or for video files, which are limited by CPU speed and 1ms min of waitKey()
+        # or for video files, which are limited by CPU speed/1ms min of waitKey()
         try:
-            fps = float( 0 if not fps else fps ) 
+            self.fps_init = float( 29.97 if not fps else fps )
         except ValueError:
-            fps = 0.0
-        
+            self.fps_init = 29.97
+
+        # size given, to compare with size of first frame
+        self.size_init = size
+
         # if source_type is 'device': Otherwise does nothing
-        self.capture.set( cv.CV_CAP_PROP_FPS, float(fps) )
-        self.capture.set( cv.CV_CAP_PROP_FRAME_WIDTH, float(size[0]) )
-        self.capture.set( cv.CV_CAP_PROP_FRAME_HEIGHT, float(size[1]) )
-           
+        if not self.source_type == 'file':
+            self.capture.set( cv.CV_CAP_PROP_FPS, float( self.fps_init ) )
+            self.capture.set( cv.CV_CAP_PROP_FRAME_WIDTH, float(self.size_init[0]) )
+            self.capture.set( cv.CV_CAP_PROP_FRAME_HEIGHT, float(self.size_init[1]) )
+
         # Grab first frame, don't append to framebuffer
         self.grab_first()
 
 
     def grab_first( self ):
-        """ Grabs first fram to get source image parameters. """
+        """ Grabs first frame to get source image parameters. """
+
+        # Possibly eternal loop until first frame returned
         rv = False
-        while not rv:   
+        while not rv:
             rv, img = self.capture.read()
-       
+
         # get source parameters
-        self.width  = int( self.capture.get(3) )
-        self.height = int( self.capture.get(4) )
-        self.fps    = int( self.capture.get(5) )
+        self.size = tuple( [int( self.capture.get(3) ), int( self.capture.get(4) )] )
+        self.fps = int( self.capture.get(5) )
         self.fourcc = self.capture.get(6)
-    
-    
+
+        if DEBUG: print 'First frame grab - fps: ' + str(self.fps) + '; size: ' + str(self.size) + ';'
+
+
     def grab_next( self ):
         """Grabs a new frame from the source and appends it to framebuffer."""
         rv, img = self.capture.read()
         if rv:
-            # time of first 'real' frame            
-            if self.framecount == 0: self.ts_first = time.clock()
+            # time of first 'real' frame
+            if self.framecount == 0:
+                self.ts_first = time.clock()
+
             self.framecount += 1
             self.ts_last_frame = time.clock()
             self.framebuffer.appendleft(img)
         return rv
-        
-        
+
+
     def close( self ):
+        print 'Closing Grabber'
         self.capture.release()
 
 
 
 if __name__ == "__main__":
-    
-    # Parsing arguments    
+
+    # Parsing arguments
     ARGDICT = docopt(__doc__, version=None)
     DEBUG = ARGDICT['--DEBUG']
     if DEBUG: print( ARGDICT )
 
-    # Width and height; WWWxHHH to tuple of floats; cv2 property set requires floats
-    if not ARGDICT['--dims']:
-        size = [0.0, 0.0]
-    else:
-        size = ARGDICT['--dims'].split('x')
-        for i,s in enumerate(size):
-            size[i] = float(s)
-        
+    # Width and height; WWWxHHH to tuple of ints; cv2 set requires floats
+    size = (0, 0) if not ARGDICT['--dims'] else tuple( ARGDICT['--dims'].split('x') )
+
+
     # instantiate main class
     main = Grabber( source = ARGDICT['--source'],
-                    fps    = ARGDICT['--fps'], 
+                    fps    = ARGDICT['--fps'],
                     size   = size )
-    
-    # Requirements for main loop    
+
+    # Requirements for main loop
     if DEBUG: print 'fps: ' + str( main.fps )
     if main.fps:
         t = int( 1000/main.fps )
     else:
         t = 1
-        
-    # Main loop
-    ts_start = time.clock()
-    while True:
-        if main.grab_next():
-            cv2.imshow( 'Main', main.framebuffer.pop() )
-        else:
-            main.close()
-            cv2.destroyAllWindows()
-            sys.exit( 0 )    
 
-        if ( cv2.waitKey( t ) % 0x100 == 27 ):
+    # Main loop
+    key = 0
+    while True:
+        if not main.grab_next() or ( key % 0x100 == 27 ):
             main.close()
             cv2.destroyAllWindows()
             sys.exit( 0 )
+        else:
+            cv2.imshow( 'Main', main.framebuffer.pop() )
+            key = cv2.waitKey( t )
