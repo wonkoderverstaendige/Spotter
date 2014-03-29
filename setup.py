843d16dd05d2894764260b6b72bd2090d98e9156
wonko <crangos@web.de>
2014-03-29 12:29:17 +0100

    Add placeholder setup.py

---
+++ setup.py
@@ -0,0 +1,5 @@
+from distutils.core import setup
+
+setup(name='Spotter', version='0.5', packages=['lib', 'lib.ui', 'lib.core', 'lib.docopt', 'lib.configobj'], url='',
+      license='', author='Ronny Eichler', author_email='ronny.eichler@gmail.com',
+      description='Spotter - simple video tracking')
